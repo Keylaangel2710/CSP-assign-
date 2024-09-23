87ce035b172cde316e1304489457ddfc696116a4
Keylaangel2710 <keyla.angel@ucas-edu.net>
2024-09-23 19:25:35 +0000

    practice in py

---
+++ Variables Pratice Folder /Pratice.py
@@ -0,0 +1,20 @@
+name = "keyla"
+print(name)
+number = "2"
+print(number)
+bignum = "250"
+print(bignum)
+Breakfeast = "waffles"
+print(Breakfeast)
+color = "pink"
+print(color)
+School = "Ucas"
+print(School)
+Year = "2024"
+print(Year)
+eyes = "brown"
+print(eyes) 
+age = "14"
+print(age)
+Subject = "drawing"
+print(Subject)
