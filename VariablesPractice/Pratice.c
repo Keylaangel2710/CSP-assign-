2153908fce69f10bc2d7fcfbba8f166826264911
Keylaangel2710 <keyla.angel@ucas-edu.net>
2024-09-26 19:16:06 +0000

    Pratice C

---
+++ VariablesPractice/Pratice.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+    char name[]= "keyla"; 
+    printf("%s\n", name);
+    char tinynumber[]= "2";
+    printf("%s\n", tinynumber);
+    char bignumber[]= "250";
+    printf("%s\n", bignumber);
+    char breakfast[]= "Waffle";
+    printf("%s\n", breakfast);
+    char color[]= "pink";
+    printf("%s\n", color);
+    char school[]= "Ucas";
+    printf("%s\n", school);
+    char year[]="2024";
+    printf("%s\n",year);
+    char eye[]="brown";
+    printf("%s\n", eye);
+    char age[]="14";
+    printf("%s\n", age);
+    char subject[]="Drawing";
+    printf("%s\n", subject);
+    return 0;
+}
