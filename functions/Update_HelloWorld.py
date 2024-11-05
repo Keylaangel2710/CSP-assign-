f35b8c7cdb9ea74edbe7750fbdcc4385942d64a2
Keylaangel2710 <keyla.angel@ucas-edu.net>
2024-11-05 06:52:40 +0000

    Update_Helloworld.py

---
+++ functions/Update_HelloWorld.py
@@ -0,0 +1,11 @@
+def name(name):
+    return f"Hello {name}"
+print(name("lindsey!"))
+      
+print(name("Melissa!"))
+            
+print(name("Isabella!"))
+
+print(name("Danny!"))
+
+print(name("leticia!"))
