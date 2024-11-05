76afcae0ffce277e858d0a24daa76df72a5a471c
Keylaangel2710 <keyla.angel@ucas-edu.net>
2024-11-05 07:08:22 +0000

    update_helloworld.c

---
+++ functions/update_Helloworld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void name (char name[]){
+    printf("Hello %s",name);
+}
+int main (void){
+name ("Danny!\n");
+name("Isabella!\n");
+name("Sydney!\n");
+name("Sammy!\n");
+name("raul!\n");
+
+
+}
