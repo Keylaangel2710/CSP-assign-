e5e13f84374573459314edd07ce8815240663cb5
Keylaangel2710 <keyla.angel@ucas-edu.net>
2024-11-13 03:00:49 +0000

    age but older

---
+++ conditionals /oldenough.c
@@ -0,0 +1,16 @@
+#inlcude <studio.h>
+int age = 16;
+int main (void) {
+printf("Your are old enought to vote\n");
+}else if (age >= 15){
+    printf("You are old enough to drive\n");
+}else if (age ==15){
+    print("You are old enough to get your learners permit\n")
+
+}else if (age >= 6){
+    printf("You are old enough to go to school\n");
+}else{
+    printf("You are to young to go to school!");
+};
+return 0;
+}
