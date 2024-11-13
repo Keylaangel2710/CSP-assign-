881b6fc8e4f6ae59d8684e32636c6bd804625012
Keylaangel2710 <keyla.angel@ucas-edu.net>
2024-11-13 02:48:12 +0000

    time.py

---
+++ conditionals /time.py
@@ -0,0 +1,13 @@
+import datetime
+
+cureenttime = datetime.datetime.now()
+
+print(currenttime.hour)
+
+if currenttime.hour <= 12:
+    printf("Goodmorning")
+
+elif currenttime.hour <= 18:
+    printf("Good Afternoon")
+else:
+    printf("Good Evening")
