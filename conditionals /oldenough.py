5318125cac956afe2ce306c38011a4bf64808f03
Keylaangel2710 <keyla.angel@ucas-edu.net>
2024-11-13 02:53:49 +0000

    age

---
+++ conditionals /oldenough.py
@@ -0,0 +1,11 @@
+age = 6
+if age >= 18:
+    print("Your old enough to vote")
+elif age >= 17:
+    printf("YOur old enough to drive")
+elif age >= 16:
+    print("Your old enough to get your learners permit")
+elif age >= 5:
+    print("Your old enough to go to school")
+else:
+    print("You are to young to go to school")
