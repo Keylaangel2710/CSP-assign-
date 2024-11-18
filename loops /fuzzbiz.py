346775d4e3e026c48a72250b634fe943890b977a
Keylaangel2710 <keyla.angel@ucas-edu.net>
2024-11-18 05:24:27 +0000

    Fiizzbuzz. py

--- loops /fuzzbiz.py
+++ loops /fuzzbiz.py
@@ -1,4 +1,11 @@
 i=0
 while i <50:
     i=i+1
-    if 
+    if i%3==0 and i%5==0;
+        print("fizzbuzz")
+    elif i%3==0:
+        print("fizz")
+    elif 1%5==0:
+        print("buzz")
+    else:
+        print(i)
