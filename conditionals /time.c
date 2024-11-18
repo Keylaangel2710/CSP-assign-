fc30bc891d51fa354367847d6d84f5149b8f478a
Keylaangel2710 <keyla.angel@ucas-edu.net>
2024-11-18 00:15:57 +0000

    real time.c

--- conditionals /time.c
+++ conditionals /time.c
@@ -1,23 +1,27 @@
-#include <studio.h>
+#include <stdio.h>
 #include <time.h>
-
 
 int hour;
 
 int main()
 {
-time_t now; 
-struct tm * now_tm;
+    time_t now; 
+    struct tm * now_tm;
 
 now = time (NULL);
 now_tm = localtime (&now);
 hour = (now_tm->tm_hour);
 printf("%d\n", hour);
+
 if (hour < 12) {
     printf("Good morning\n");
-}else if (hour < 18"){
+}else if (hour < 18){
     printf ("Good afternoon\n");
-    
+
+    }else{
+        printf("Good evening\n");
     }
 
 return 0;
+
+}
